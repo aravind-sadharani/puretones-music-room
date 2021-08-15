67d7309d3bdc87d6c610866057b1bb050a22eabc
Aravind Iyer <aravind.iyer@chandraveena.com>
2021-08-15 20:03:30 +0530

    Rename puretones.min.js to bypass stale cache

--- src/services/faust.js
+++ src/services/faust.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet"
 const IncludeFaust = () => (
     <Helmet>
         <script src="/Faustlib/index.min.js" type="text/javascript"></script>
-        <script src="/Faustlib/puretones.min.js" type="text/javascript"></script>
+        <script src="/Faustlib/puretones-comb.min.js" type="text/javascript"></script>
         <script src="/Faustlib/musicscale.min.js" type="text/javascript"></script>
         <script src="/Faustlib/musicscale-worklet.min.js" type="text/javascript"></script>
     </Helmet>
