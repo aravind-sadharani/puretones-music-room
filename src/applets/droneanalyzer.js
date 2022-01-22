cb391d26c15fd7f62d22a7b9f4aa26d9d4d71c86
Aravind Iyer <aravind.iyer@chandraveena.com>
2022-01-22 16:56:04 +0530

    Add drone analyzer

---
+++ src/applets/droneanalyzer.js
@@ -0,0 +1,249 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+import TabNav from "components/tabs"
+import Selector from "components/selector"
+import Slider from "components/slider"
+import Button from 'components/button'
+import {analyzeDrone} from 'utils/analyzedrone'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const DroneAnalyzerContainer = styled.div`
+    padding: 12px 12px 0 12px;
+    margin: 0 0 1em 0;
+    border: 1px solid;
+    border-color: ${({theme}) => theme.light.borderColor};
+    ${({theme}) => theme.isDark`border-color: ${theme.dark.borderColor};`}
+    border-radius: 5px;
+`
+
+const DroneAnalyzerResultElement = styled.pre`
+    margin: 0 0 1em 0;
+    overflow-x: scroll;
+`
+
+const chartOptions = {
+    responsive: true,
+    scales: {
+        yAxes: {
+            title: {
+                display: true,
+                text: 'Count',
+                font: {
+                    size: 15,
+                }
+            }
+        },
+        xAxes: {
+            title: {
+                display: true,
+                text: 'Ratio',
+                font: {
+                    size: 15,
+                }  
+            }
+        },
+    },
+    plugins: {
+        legend: {
+            display: false,
+            position: 'top',
+        },
+        title: {
+            display: false,
+            text: 'Drone Pitches',
+        }
+    },
+}
+
+const stringSelectParams = {
+    key: "Note",
+    options: [
+      {
+        value: "0",
+        text: "SA"
+      },
+      {
+        value: "1",
+        text: "Ni"
+      },
+      {
+        value: "2",
+        text: "ni"
+      },
+      {
+        value: "3",
+        text: "Dha"
+      },
+      {
+        value: "4",
+        text: "dha"
+      },
+      {
+        value: "5",
+        text: "Pa"
+      },
+      {
+        value: "6",
+        text: "Ma"
+      },
+      {
+        value: "7",
+        text: "ma"
+      },
+      {
+        value: "8",
+        text: "Ga"
+      },
+      {
+        value: "9",
+        text: "ga"
+      },
+      {
+        value: "10",
+        text: "Re"
+      },
+      {
+        value: "11",
+        text: "re"
+      },
+      {
+        value: "12",
+        text: "Sa"
+      }
+    ]
+}
+
+const fineTuneParams = {
+    key: "Fine Tune",
+    init: 0,
+    max: 100,
+    min: -100,
+    step: 1
+}
+
+const ultraFineTuneParams = {
+    key: "Ultrafine Tune",
+    init: 0,
+    max: 100,
+    min: -100,
+    step: 1
+}
+
+const DroneAnalyzer = () => {
+    const [droneState,setDroneState] = React.useState({
+        strings: [
+            {
+                value: 5,
+                finetune: 0,
+                ultrafinetune: 0,
+            },
+            {
+                value: 0,
+                finetune: 0,
+                ultrafinetune: 0,
+            },
+            {
+                value: 12,
+                finetune: 0,
+                ultrafinetune: 0,
+            }
+        ]
+    })
+
+    const onDroneStateChange = (value,path) => {
+        let newStrings = droneState.strings
+        if(path.includes('select')) {
+            newStrings[Number(path.replace("select",""))].value = Number(value)
+        } else if(path.includes('fine')) {
+            newStrings[Number(path.replace("fine",""))].finetune = Number(value)
+        } else if(path.includes('ultra')) {
+            newStrings[Number(path.replace("ultra",""))].ultrafinetune = Number(value)
+        }
+        setDroneState({strings: newStrings})
+    }
+
+    const stringTabs = ['String 1', 'String 2', 'String 3']
+
+    const stringConfig = droneState.strings.map((string,index) => {
+        const selectorParams = {...stringSelectParams,
+            default: `${string.value}`,
+        }
+        const fineParams = {...fineTuneParams,
+            init: `${string.finetune}`,
+        }
+        const ultraFineParams = {...ultraFineTuneParams,
+            init: `${string.ultrafinetune}`,
+        }
+        let path = `${index}`
+        return (
+            <>
+                <Selector params={selectorParams} path={`select${path}`} onParamUpdate={onDroneStateChange} />
+                <Slider params={fineParams} path={`fine${path}`} onParamUpdate={onDroneStateChange} />
+                <Slider params={ultraFineParams} path={`ultra${path}`} onParamUpdate={onDroneStateChange} />
+            </>
+        )
+    })
+
+    const [droneAnalysis,setDroneAnalysis] = React.useState({status: false, message: ""})
+    const [dronePitches,setDronePitches] = React.useState({})
+
+    const analyze = () => {
+        let result = analyzeDrone(droneState.strings)
+
+        setDroneAnalysis({status: result.status, message: result.message})
+        setDronePitches({
+            labels: result.pitches.map((pitch) => (2**(pitch.ratio/1200)).toFixed(5)),
+            datasets: [
+                {
+                    label: 'Generated Pitches',
+                    data: result.pitches.map((pitch) => pitch.count),
+                    backgroundColor: '#f98ca4',
+                },
+            ],
+        })
+    }
+
+    return (
+        <>
+            <DroneAnalyzerContainer>
+                <p><strong>Drone Analyzer</strong></p>
+                <TabNav tablist={stringTabs} pagelist={stringConfig} />
+                <center>
+                    <Button onClick={() => analyze()}>Analyze</Button>
+                </center>
+            </DroneAnalyzerContainer>
+            {droneAnalysis.status && <DroneAnalyzerContainer>
+                <p><strong>Pitches Generated by the Drone</strong></p>
+                <Bar options={chartOptions} data={dronePitches} />
+            </DroneAnalyzerContainer>}
+            {droneAnalysis.message !== '' && <DroneAnalyzerContainer>
+                <p><strong>Detailed Results</strong></p>
+                <DroneAnalyzerResultElement>
+                        <code>
+                            {droneAnalysis.message}
+                        </code>
+                </DroneAnalyzerResultElement>
+            </DroneAnalyzerContainer>}
+        </>
+    )
+}
+
+export default DroneAnalyzer
---
+++ src/utils/analyzedrone.js
@@ -0,0 +1,88 @@
+const MAXHARMONICS = 64
+const OCTAVE = 1200
+const EPSILON = 0.001
+const DELTACENTS = 4
+
+const GCD = (a,b) => {
+    if(a === 0)
+        return b
+    if(b === 0)
+        return a
+    if(a > b)
+        return GCD(b,a % b)
+    else
+        return GCD(a,b % a)
+}
+
+const isAudible = ratio => (ratio > 1/4 - EPSILON) && (ratio < 64 + EPSILON)
+
+const toCents = ratio => {
+    let baseCents = OCTAVE*Math.log2(ratio)
+    while(baseCents < 0)
+        baseCents += OCTAVE
+    return baseCents % OCTAVE
+}
+
+const stringRatios = [2,243/128,16/9,27/16,128/81,3/2,729/512,4/3,81/64,32/27,9/8,256/243,1]
+const stringNames = ['SA','Ni','ni','Dha','dha','Pa','Ma','ma','Ga','ga','Re','re','Sa']
+
+const analyzeDrone = (stringConfig) => {
+    let status = true
+    let message = `Drone Analysis with three strings tuned as ${stringNames[Number(stringConfig[0].value)]}, ${stringNames[Number(stringConfig[1].value)]} and ${stringNames[Number(stringConfig[2].value)]}\n\n`
+
+    let strings = [0,1,2].map(i => stringRatios[Number(stringConfig[i].value)]*(2**((Number(stringConfig[i].finetune) + Number(stringConfig[i].ultrafinetune)/100)/1200)))
+
+    let relevantTones = []
+
+    const addRatio = ratio => {
+        if(ratio === 0 || !isAudible(ratio))
+            return
+                                
+        let existingRatio = relevantTones.find(tone => (Math.abs(tone.ratio - toCents(ratio)) < DELTACENTS))
+        if( existingRatio !== undefined) {
+            existingRatio.ratio = (existingRatio.ratio*existingRatio.count + toCents(ratio))/(existingRatio.count + 1)
+            existingRatio.count += 1
+            return
+        }
+                
+        relevantTones.push({
+            ratio: toCents(ratio),
+            count: 1,
+        })
+    }
+
+    for(let i = 1; i <= MAXHARMONICS; i++) {
+        strings.forEach(string => {
+            let ratio = i*string
+            addRatio(ratio)
+        })
+        for(let j = 1; j<= MAXHARMONICS; j++) {
+            if(GCD(i,j) !== 1)
+                continue
+            
+            let stringPairs = [
+                [strings[0],strings[1]],
+                [strings[1],strings[2]],
+                [strings[2],strings[0]]
+            ]
+
+            stringPairs.forEach(pair => {
+                let ratio = Math.abs(i*pair[0]-j*pair[1])/2
+                addRatio(ratio)
+            })
+        }
+    }
+
+    relevantTones.sort((tone1, tone2) => tone1.ratio - tone2.ratio)
+    let relevantTonesPrintable = relevantTones.map(tone => `Ratio = ${(2**(tone.ratio/1200)).toFixed(5)} (${tone.ratio.toFixed(2)} ¢)\t\t Count = ${tone.count}`).join('\n')
+    
+    message = message.concat(relevantTonesPrintable)
+
+    return {
+        status: status,
+        message: message,
+        pitches: relevantTones,
+    }
+}
+
+export {analyzeDrone}
