79dd22cf0c1436698a9275471d17be454d1972ba
Aravind Iyer <aravind.iyer@chandraveena.com>
2022-02-05 18:52:24 +0530

    Remove background added by gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,8 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 720
+              maxWidth: 720,
+              backgroundColor: 'none',
             },
           },
           `gatsby-remark-copy-linked-files`,
