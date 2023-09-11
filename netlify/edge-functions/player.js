c8b4ed6ce8a53883b1caebe6d5c3b82bcf0d7a19
Aravind Iyer <aravind.iyer@chandraveena.com>
2023-09-11 12:09:15 +0530

    Update oembed link header for discovery

--- netlify/edge-functions/player.js
+++ netlify/edge-functions/player.js
@@ -1,8 +1,11 @@
 export default async (request,context) => {
     const url = new URL(request.url)
     url.pathname = "/embed/"
-    const oembedJSONLink = `${url.origin}/oembed/?format=json&url=${encodeURIComponent(`${url}`)}`
-    const oembedXMLLink = `${url.origin}/oembed/?format=xml&url=${encodeURIComponent(`${url}`)}`
+    const queryParameters = new URLSearchParams(url.searchParams)
+    const appname = queryParameters.get('appname')
+    const title = queryParameters.get('title')
+    const oembedJSONLink = `${url.origin}/oembed/?format=json&url=${encodeURIComponent(`${url}`)}; rel="alternate"; type="application/json+oembed"; title="${title} - ${appname}"`
+    const oembedXMLLink = `${url.origin}/oembed/?format=xml&url=${encodeURIComponent(`${url}`)}; rel="alternate"; type="text/xml+oembed"; title="${title} - ${appname}"`
 
     const response = await context.next()
     response.headers.append("Link",oembedJSONLink)
